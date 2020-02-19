19a19b71f2e6b89b1ce082fca72f2f4fd2fc657c
Ankit Ghanghas <aghangha@scholar-fe05.rcac.purdue.edu>
2020-02-19 15:47:34 -0500

    added comments to the scripts

--- Lab04/dir_info.sh
+++ Lab04/dir_info.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 # Gives info about the directory
-file=`ls -l $1 | grep "^-" | wc -l`
-folder=`ls -l $1 | grep "^d" | wc -l`
-largest=`find $1 -type f -printf "%p\n" | sort -n | tail -1`
-latest=`ls $1 -Art | tail  -n 1`
+file=`ls -l $1 | grep "^-" | wc -l` # grep groups all the files starting with - and wc -l counts the number of lines- so in a way the total number of files.
+folder=`ls -l $1 | grep "^d" | wc -l` # similar to above, grep here groups all the folders using "^d"
+largest=`find $1 -type f -printf "%p\n" | sort -n | tail -1` # printf prints the file names and sort -n sorts them according to size and tail -1 picks the last largest file in the sorted list
+latest=`ls $1 -Art | tail  -n 1` # -Art sorts file by their last time modified and tail picks up the last giving most recent file
 owners=`find $1 -printf '%u:%g\n' | sort -t: -u`
 echo There are $file files in the $1 directory
 echo There are $folder directories/folders in $1 directory
--- Lab04/my_backup.sh
+++ Lab04/my_backup.sh
@@ -2,16 +2,16 @@
 # Backs up the full directory listing  in single project directory
 # Ankit Ghanghas
 
-if [ ! -d $1 ]
+if [ ! -d $1 ] # check if the directory listed as arguments exists or not
 then
 	echo 'The given directory does not exist'
 	exit
 fi
 
-date=`date +\%F`
-mkdir ~/$1_backup_$date
+date=`date +\%F` # gets todays date
+mkdir ~/$1_backup_$date # make a directory to store the backup
 
-pd=`find . -type d -name $1`
-cp -R $pd  ~/$1_backup_$date
+pd=`find . -type d -name $1` # finds the exact path to the specified directory
+cp -R $pd  ~/$1_backup_$date # copies all the files from specified directory to the backup directort
 
 echo Backup of $1 completed 
